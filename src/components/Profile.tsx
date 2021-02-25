afcf265b50d0b334c30334bc4d0c240587a16a2c
Guilherme Santos <santosgui678@gmail.com>
2021-02-25 09:55:08 -0300

    Estrutura de Leveling + Notificação com áudio

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/guisantosfr.png" alt="Guilherme Santos" />
@@ -8,8 +12,8 @@ export function Profile() {
         <strong>Guilherme Santos</strong>
         <p>
           <img src='icons/level.svg' alt="level" />
-          Level 1
-          </p>
+          Level {level}
+        </p>
       </div>
     </div>
   );
